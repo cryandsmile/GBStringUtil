db42979592ce18fb5142afcb31aa86410369e7d2
gaobin <gexiaotian5@gmail.com>
2021-04-26 16:33:59 +0800

    update

--- GBStringUtil/Classes/GBStringUtil.h
+++ GBStringUtil/Classes/GBStringUtil.h
@@ -11,6 +11,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GBStringUtil : NSObject
 + (NSString *)UUID;
+
+//MD5加密
++ (NSString *)md5:(NSString *)input;
+
+//去空格
++ (NSString *)replaceSpace:(NSString *)originString;
 @end
 
 NS_ASSUME_NONNULL_END
--- GBStringUtil/Classes/GBStringUtil.m
+++ GBStringUtil/Classes/GBStringUtil.m
@@ -6,6 +6,7 @@
 //
 
 #import "GBStringUtil.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @implementation GBStringUtil
 
@@ -13,4 +14,25 @@
     return @"";
 }
 
+//MD5加密
++ (NSString *)md5:(NSString *)input {
+    if (!input) {
+           return nil;
+       }
+       
+       const char *cStr = [input UTF8String];
+       unsigned char result[16];
+       CC_MD5(cStr, (CC_LONG)strlen(cStr), result); // This is the md5 call
+       return [NSString stringWithFormat:
+               @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+               result[0], result[1], result[2], result[3],
+               result[4], result[5], result[6], result[7],
+               result[8], result[9], result[10], result[11],
+               result[12], result[13], result[14], result[15]
+               ];
+}
+//去空格
++ (NSString *)replaceSpace:(NSString *)originString {
+    return [originString stringByReplacingOccurrencesOfString:@" " withString:@""];
+}
 @end
